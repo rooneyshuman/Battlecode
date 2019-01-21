e06abb07b1846eb14b93b55c400e83e1eb63a78f
Aaron Chan <aachan@pdx.edu>
2019-01-21 13:38:46 -0800

    Add random unit location building

--- build/robot.js
+++ build/robot.js
@@ -6,27 +6,50 @@ class MyRobot extends BCAbstractRobot {
     this.step = 0;
   }
   turn() {
+    const adj_choices = [
+      [0, -1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1, 1],
+      [-1, 0],
+      [-1, -1],
+    ];
     this.step++;
+    if (this.me.unit === SPECS.PILGRIM) {
+      this.log(`Pilgrim health: ${this.me.health}`);
+    }
+    if (this.me.unit === SPECS.PREACHER) {
+      this.log(`Preacher health: ${this.me.health}`);
+    }
     if (this.me.unit === SPECS.CRUSADER) {
       this.log(`Crusader health: ${this.me.health}`);
-      const choices = [
-        [0, -1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-        [0, 1],
-        [-1, 1],
-        [-1, 0],
-        [-1, -1],
-      ];
-      const choice = choices[Math.floor(Math.random() * choices.length)];
+      const choice =
+        adj_choices[Math.floor(Math.random() * adj_choices.length)];
       return this.move(choice[0], choice[1]);
     }
     if (this.me.unit === SPECS.CASTLE) {
       this.log(`Castle health: ${this.me.health}`);
       if (this.step % 10 === 0) {
-        this.log(`Building a crusader at (${this.me.x + 1}, ${this.me.y + 1})`);
-        return this.buildUnit(SPECS.CRUSADER, 1, 1);
+        let i = 0;
+        while (
+          !this.map[adj_choices[i][1]][adj_choices[i][0]] &&
+          i < adj_choices.length
+        ) {
+          // Makes sure the terrain is passable.
+          // this.map is indexed as [y][x]
+          i++;
+        }
+        this.log(
+          `Building a crusader at (${this.me.x + adj_choices[i][0]}, ${this.me
+            .y + adj_choices[i][1]})`,
+        );
+        return this.buildUnit(
+          SPECS.CRUSADER,
+          adj_choices[i][0],
+          adj_choices[i][1],
+        );
       }
     }
   }
--- src/MyRobot.ts
+++ src/MyRobot.ts
@@ -4,23 +4,30 @@ class MyRobot extends BCAbstractRobot {
   private step = 0;
 
   public turn(): Action | Falsy {
+    const adjChoices = [
+      [0, -1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1, 1],
+      [-1, 0],
+      [-1, -1],
+    ];
+
     this.step++;
+    if (this.me.unit === SPECS.PILGRIM) {
+      this.log(`Pilgrim health: ${this.me.health}`);
+    }
+
+    if (this.me.unit === SPECS.PREACHER) {
+      this.log(`Preacher health: ${this.me.health}`);
+    }
 
     if (this.me.unit === SPECS.CRUSADER) {
       this.log(`Crusader health: ${this.me.health}`);
 
-      const choices = [
-        [0, -1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-        [0, 1],
-        [-1, 1],
-        [-1, 0],
-        [-1, -1],
-      ];
-
-      const choice = choices[Math.floor(Math.random() * choices.length)];
+      const choice = adjChoices[Math.floor(Math.random() * adjChoices.length)];
       return this.move(choice[0], choice[1]);
     }
 
@@ -28,8 +35,24 @@ class MyRobot extends BCAbstractRobot {
       this.log(`Castle health: ${this.me.health}`);
 
       if (this.step % 10 === 0) {
-        this.log(`Building a crusader at (${this.me.x + 1}, ${this.me.y + 1})`);
-        return this.buildUnit(SPECS.CRUSADER, 1, 1);
+        let i = 0;
+        while (
+          !this.map[adjChoices[i][1]][adjChoices[i][0]] &&
+          i < adjChoices.length
+        ) {
+          // Makes sure the terrain is passable.
+          // this.map is indexed as [y][x]
+          i++;
+        }
+        this.log(
+          `Building a crusader at (${this.me.x + adjChoices[i][0]}, ${this.me
+            .y + adjChoices[i][1]})`,
+        );
+        return this.buildUnit(
+          SPECS.CRUSADER,
+          adjChoices[i][0],
+          adjChoices[i][1],
+        );
       }
     }
   }
