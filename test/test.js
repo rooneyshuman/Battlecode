0fb39617b2915191cd55f0452856d7c6399d0ff5
Aaron Chan <aachan@pdx.edu>
2019-01-21 14:04:21 -0800

    Abandon testing
    Unsure of testing with the external battlecode library.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should;
+// var robot = require("../build/robot");
+
+it('Should equal 0', function() {
+  assert.equal(1 - 1, 0);
+});
