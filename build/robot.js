6f14594561558c18f5299b3c37e7ed2161b90ce6
Aaron Chan <aachan@pdx.edu>
2019-01-21 16:30:32 -0800

    Add random movement for units
    Only checks if the terrain is passable.

--- build/robot.js
+++ build/robot.js
@@ -4,9 +4,7 @@ class MyRobot extends BCAbstractRobot {
   constructor() {
     super(...arguments);
     this.step = 0;
-  }
-  turn() {
-    const adjChoices = [
+    this.adjChoices = [
       [0, -1],
       [1, -1],
       [1, 0],
@@ -16,35 +14,80 @@ class MyRobot extends BCAbstractRobot {
       [-1, 0],
       [-1, -1],
     ];
+  }
+  turn() {
     this.step++;
-    if (this.me.unit === SPECS.PILGRIM);
-    if (this.me.unit === SPECS.PREACHER);
-    if (this.me.unit === SPECS.CRUSADER);
-    if (this.me.unit === SPECS.CASTLE) {
-      // this.log(`Castle health: ${this.me.health}`);
-      if (this.step % 10 === 0) {
-        let i = 0;
-        while (
-          !this.map[this.me.y + adjChoices[i][1]][
-            this.me.x + adjChoices[i][0]
-          ] &&
-          i < adjChoices.length
-        ) {
-          // Makes sure the terrain is passable.
-          // this.map is indexed as [y][x]
-          i++;
-        }
-        this.log(
-          `Building a crusader at (${this.me.x + adjChoices[i][0]}, ${this.me
-            .y + adjChoices[i][1]})`,
-        );
-        return this.buildUnit(
-          SPECS.CRUSADER,
-          adjChoices[i][0],
-          adjChoices[i][1],
-        );
+    switch (this.me.unit) {
+      case SPECS.PILGRIM: {
+        this.log('Pilgrim');
+        break;
+      }
+      case SPECS.PREACHER: {
+        this.log('Preacher');
+        // Add unit handling in another function
+        break;
+      }
+      case SPECS.CRUSADER: {
+        // this.log(`Crusader health: ${this.me.health}`);
+        const choice = this.randomValidLoc();
+        return this.move(choice[0], choice[1]);
+      }
+      case SPECS.CASTLE: {
+        return this.handleCastle();
       }
     }
+  }
+  handleCastle() {
+    // this.log(`Castle health: ${this.me.health}`);
+    if (this.step % 10 === 0) {
+      const buildLoc = this.simpleValidLoc();
+      this.log(`Building a crusader at (${buildLoc[0]}, ${buildLoc[1]})`);
+      return this.buildUnit(SPECS.CRUSADER, buildLoc[0], buildLoc[1]);
+    }
+  }
+  randomValidLoc() {
+    // FIXME: Fix for map edges.
+    const mapDim = this.map[0].length;
+    let rand = Math.floor(Math.random() * this.adjChoices.length);
+    let loc = this.adjChoices[rand];
+    let counter = 0;
+    do {
+      if (this.me.y + loc[1] >= mapDim) {
+        loc[1] = -1;
+      }
+      if (this.me.y + loc[1] < 0) {
+        loc[1] = 1;
+      }
+      if (this.me.x + loc[0] >= mapDim) {
+        loc[0] = -1;
+      }
+      if (this.me.x + loc[0] < 0) {
+        loc[0] = 1;
+      }
+      rand = (rand + 1) % this.adjChoices.length;
+      counter++;
+    } while (
+      !this.map[this.me.y + loc[1]][this.me.x + loc[0]] &&
+      counter < this.adjChoices.length
+    );
+    if (counter >= this.adjChoices.length) {
+      loc = [0, 0];
+    }
+    return loc;
+  }
+  simpleValidLoc() {
+    let i = 0;
+    while (
+      !this.map[this.me.y + this.adjChoices[i][1]][
+        this.me.x + this.adjChoices[i][0]
+      ] &&
+      i < this.adjChoices.length
+    ) {
+      // Makes sure the terrain is passable.
+      // this.map is indexed as [y][x]
+      i++;
+    }
+    return this.adjChoices[i];
   }
 }
 // Prevent Rollup from removing the entire class for being unused
--- src/MyRobot.ts
+++ src/MyRobot.ts
@@ -18,21 +18,23 @@ class MyRobot extends BCAbstractRobot {
     switch(this.me.unit){
       case SPECS.PILGRIM: {
         this.log("Pilgrim");
+        break;
       }
       
       case SPECS.PREACHER: {
         this.log("Preacher");
         // Add unit handling in another function
+        break;
       }
       case SPECS.CRUSADER: {
         // this.log(`Crusader health: ${this.me.health}`);
 
-        const choice = this.adjChoices[Math.floor(Math.random() * this.adjChoices.length)];
-        // return this.move(choice[0], choice[1]);
+        const choice: number[] = this.randomValidLoc();
+        return this.move(choice[0], choice[1]);
       }
 
       case SPECS.CASTLE: {
-        this.handleCastle()
+        return this.handleCastle()
       }
     }
   }
@@ -40,17 +42,44 @@ class MyRobot extends BCAbstractRobot {
   private handleCastle(): Action | Falsy {
     // this.log(`Castle health: ${this.me.health}`);
     if (this.step % 10 === 0) {
-      const buildLoc: number[] = this.unitBuildLocation()
+      const buildLoc: number[] = this.simpleValidLoc()
       this.log(`Building a crusader at (${buildLoc[0]}, ${buildLoc[1]})`,);
       return this.buildUnit(SPECS.CRUSADER, buildLoc[0], buildLoc[1]);
     }
   }
+  private randomValidLoc(): number[] {
+    // FIXME: Fix for map edges.
+    const mapDim = this.map[0].length
+    let rand = Math.floor(Math.random() * this.adjChoices.length);
+    let loc = this.adjChoices[rand];
+    let counter = 0;
 
-  private unitBuildLocation(): number[] {
+    do {
+      if(this.me.y + loc[1] >= mapDim) {
+        loc[1] = -1;
+      }
+      if(this.me.y + loc[1] < 0) {
+        loc[1] = 1;
+      }
+      if(this.me.x + loc[0] >= mapDim) {
+        loc[0] = -1;
+      }
+      if(this.me.x + loc[0] < 0) {
+        loc[0] = 1;
+      }
+      rand = (rand + 1) % this.adjChoices.length;
+      counter++;
+    } while(!this.map[this.me.y + loc[1]][this.me.x+loc[0]] && counter < this.adjChoices.length);
+    if(counter >= this.adjChoices.length) {
+      loc = [0, 0];
+    }
+    return loc;
+  }
+
+  private simpleValidLoc(): number[] {
     let i = 0;
     while (!this.map[this.me.y + this.adjChoices[i][1]][this.me.x + this.adjChoices[i][0]] && i < this.adjChoices.length) {
       // Makes sure the terrain is passable.
-      // TODO: Make sure a unit is not already on that space.
       // this.map is indexed as [y][x]
       i++;
     }
