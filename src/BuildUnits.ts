274f80dd9adc583994f970629feabd55fb4e37b9
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-01-26 17:40:59 -0800

    Added examplefuncsplayer and fixed start script to use it as blue team

--- src/BuildUnits.ts
+++ src/BuildUnits.ts
@@ -1,7 +1,7 @@
 import { BCAbstractRobot, SPECS } from 'battlecode';
 
 export function castleBuild(self: any): BuildAction | Falsy {
-    const units: number[] = [2, 3, 4, 5];
+    const units: number[] = [1, 2, 3, 4];
     const unitToBuild = units[Math.floor(Math.random() * units.length)];
 
     self.log(`Castle health: ${self.me.health}`);
@@ -9,19 +9,19 @@ export function castleBuild(self: any): BuildAction | Falsy {
     if (self.step % 10 === 0) {
         const buildLoc: number[] = self.randomValidLoc();
         switch (unitToBuild) {
-            case 2: {
+            case 1: {
                 self.log(`Building a pilgrim at (${buildLoc[0]}, ${buildLoc[1]})`);
                 return self.buildUnit(SPECS.PILGRIM, buildLoc[0], buildLoc[1]);
             }
-            case 3: {
+            case 2: {
                 self.log(`Building a crusader at (${buildLoc[0]}, ${buildLoc[1]})`);
                 return self.buildUnit(SPECS.CRUSADER, buildLoc[0], buildLoc[1]);
             }
-            case 4: {
+            case 3: {
                 self.log(`Building a prophet at (${buildLoc[0]}, ${buildLoc[1]})`);
                 return self.buildUnit(SPECS.PROPHET, buildLoc[0], buildLoc[1]);
             }
-            case 5: {
+            case 4: {
                 self.log(`Building a preacher at (${buildLoc[0]}, ${buildLoc[1]})`);
                 return self.buildUnit(SPECS.PREACHER, buildLoc[0], buildLoc[1]);
             }
