1cb1e4dd6fd5eab0c52b018e8df365cdef160beb
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-02-04 21:35:44 -0800

    Changed mining location test to work with updated method

--- test/MyRobot.test.ts
+++ test/MyRobot.test.ts
@@ -1,16 +1,11 @@
+import { closestCoords, closestMiningLocation, fillArray, simplePathFinder } from "../src/utils";
 import { PriorityQueue } from "../src/PriorityQueue";
-import { closestCoords, fillArray, miningLocations,  simplePathFinder } from "../src/utils";
-
 // let robot = new MyRobot();
 
-test("Mining Locations", () => {
-    const testMap = [[true, true, false], [false, true, false], [false, false, true]]
-    const locations = miningLocations(testMap)
-    expect(locations).toHaveLength(4);
-    expect(locations[0]).toEqual([0, 0]);
-    expect(locations[1]).toEqual([1, 0]);
-    expect(locations[2]).toEqual([1, 1]);
-    expect(locations[3]).toEqual([2, 2]);
+test("Closest Mining Location", () => {
+    const testMap = [[true, true, false], [false, false, false], [false, false, true]]
+    const location = closestMiningLocation([2,1], testMap)
+    expect(location).toEqual([2, 2]);
 })
 
 test("Closest Coords", () => {
