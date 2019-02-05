00a62d4748ab8f149a35d4f625e519125409792f
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-02-04 21:42:38 -0800

    Fixed jest setting to be able to run tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'd.ts'],
+  moduleDirectories: ['<rootDir>/src', 'node_modules'],
+  modulePaths: ['<rootDir>/src', 'node_modules'],
 };
