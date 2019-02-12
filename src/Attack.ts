60fb1d6823ebded5421f319db210332655e32c9a
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-02-12 14:07:48 -0800

    Reformatted and cleaned up code

--- src/Attack.ts
+++ src/Attack.ts
@@ -1,10 +1,6 @@
-import {BCAbstractRobot, SPECS} from 'battlecode';
-import {closestCoords, closestMiningLocation, enemyCastle, findClosestFriendlyCastles, horizontalFlip, randomValidLoc, simplePathFinder, simpleValidLoc} from "./utils";
+import { SPECS } from 'battlecode';
 
-// const Attack = {};
-// Attack.attackFirst = (self) => 
-export function attackFirst(self: any) 
-{
+export function attackFirst(self: any) {
 	// Get all visible robots within the robots vision radius
 	const visibleRobots = self.getVisibleRobots();
 
@@ -14,20 +10,16 @@ export function attackFirst(self: any)
 	let i;
 	const robotToAttack = new Array(2);
 	let priorityRobot = -1;
-	for(i = 0; i < listLength; ++ i)
-	{
+	for (i = 0; i < listLength; ++i) {
 		const rob = visibleRobots[i];
 		// Check if the robot just showed up because of radio broadcast
-		if(!self.isVisible(rob))
-		{continue;}
+		if (!self.isVisible(rob)) { continue; }
 		// Check if robot is friendly 
-		if(self.me.team === rob.team)
-		{continue;}
+		if (self.me.team === rob.team) { continue; }
 		self.log("ROBOT: " + rob.id + " is an enemy within vision");
-		
-        const dist = (rob.x - self.me.x)**2 + (rob.y - self.me.y)**2;
-		if(SPECS.UNITS[self.me.unit].ATTACK_RADIUS[0] <= dist && dist <= SPECS.UNITS[self.me.unit].ATTACK_RADIUS[1])
-		{
+
+		const dist = (rob.x - self.me.x) ** 2 + (rob.y - self.me.y) ** 2;
+		if (SPECS.UNITS[self.me.unit].ATTACK_RADIUS[0] <= dist && dist <= SPECS.UNITS[self.me.unit].ATTACK_RADIUS[1]) {
 			self.log("CAN ATTACK ROBOT:" + rob.id);
 			// the priority of the robot that is within attacking vision if it is higher than the current one switch over to that robot
 			let priority = 0;
@@ -48,47 +40,50 @@ export function attackFirst(self: any)
 					priority = 3;
 				}
 			}
-			
-			if(priority > priorityRobot){
+
+			if (priority > priorityRobot) {
 				robotToAttack[0] = rob.x - self.me.x;
 				robotToAttack[1] = rob.y - self.me.y;
 				priorityRobot = priority;
 			}
 		}
-	}	
-	if(priorityRobot === -1)
-	{
+	}
+	if (priorityRobot === -1) {
 		return null;
 	}
 	return robotToAttack;
 }
 
-
-export function rushCastle(self: any, dest: number[], destQ: number[][])
-{
+/**
+ * Rushes robot castle
+ * @param self 
+ * @param dest 
+ * @param destQ 
+ */
+export function rushCastle(self: any, dest: number[], destQ: number[][]) {
 	let nextMove: number[];
 	self.log("DSAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 	nextMove = destQ.pop();
-	self.log("LOOOK HERE" + nextMove[0] +", " + nextMove[1]);
+	self.log("LOOOK HERE" + nextMove[0] + ", " + nextMove[1]);
 	self.log("DDSADASD " + self.me.x + ", " + self.me.y);
 
-    if(destQ.length !== 0 && ((self.me.x === nextMove[0]) && (self.me.y === nextMove[1]))) {
-    // If the destination queue has coordinates and my current location is the 
-    // same as my next move's location, then pop next destination and set nextMove to it.
-    nextMove = destQ.pop();
-    const moveX = nextMove[0] - self.me.x;
-    const moveY = nextMove[1] - self.me.y;
-    self.log(`* * * * * MOVING ${moveX}, ${moveY} > > >`)
-    return self.move(moveX, moveY);
-  }
-  else {
-	const moveX = nextMove[0] - self.me.x;
-    const moveY = nextMove[1] - self.me.y;
-    self.log(`**** ME ${self.me.x}, ${self.me.y} > > >`)
-    self.log(`***** nextMove ${nextMove} > > >`)
-    self.log(`*(**** MOVING ${moveX}, ${moveY} > > >`)
-    self.log(`****DEST ${dest} > > >`)
-    return self.move(moveX, moveY);
-  }
+	if (destQ.length !== 0 && ((self.me.x === nextMove[0]) && (self.me.y === nextMove[1]))) {
+		// If the destination queue has coordinates and my current location is the 
+		// same as my next move's location, then pop next destination and set nextMove to it.
+		nextMove = destQ.pop();
+		const moveX = nextMove[0] - self.me.x;
+		const moveY = nextMove[1] - self.me.y;
+		self.log(`* * * * * MOVING ${moveX}, ${moveY} > > >`)
+		return self.move(moveX, moveY);
+	}
+	else {
+		const moveX = nextMove[0] - self.me.x;
+		const moveY = nextMove[1] - self.me.y;
+		self.log(`**** ME ${self.me.x}, ${self.me.y} > > >`)
+		self.log(`***** nextMove ${nextMove} > > >`)
+		self.log(`*(**** MOVING ${moveX}, ${moveY} > > >`)
+		self.log(`****DEST ${dest} > > >`)
+		return self.move(moveX, moveY);
+	}
 
 }
--- src/MyRobot.ts
+++ src/MyRobot.ts
@@ -1,45 +1,29 @@
 import { BCAbstractRobot, SPECS } from 'battlecode';
 import { attackFirst, rushCastle } from "./Attack";
 import { castleBuild, pilgrimBuild } from './BuildUnits';
-import {closestCoords, closestMiningLocation, enemyCastle, findClosestFriendlyCastles, horizontalFlip, manhatDist, randomValidLoc, simplePathFinder, simpleValidLoc, visiblePilgrims} from "./utils";
+import { availableLoc, closestMiningLocation, enemyCastle, findClosestFriendlyCastles, horizontalFlip, manhatDist, randomValidLoc, simplePathFinder, simpleValidLoc, visiblePilgrims} from "./utils";
 
 class MyRobot extends BCAbstractRobot {
-  private readonly adjChoices: number[][] = [
-      [0, -1],
-      [1, -1],
-      [1, 0],
-      [1, 1],
-      [0, 1],
-      [-1, 1],
-      [-1, 0],
-      [-1, -1],
-    ];
-  
-  private storageLoc: number[][] = [];
-  // private karboniteLocation: number[] = undefined;
-  // private fuelLocation: number[] = undefined;
   private resourceLocation: number[] = undefined;
   private goMining: boolean = false;
   private destinationQueue: number[][] = [];
   private destination: number[] = undefined;
   private nextMove: number[] = undefined;
-  private enemyCastleLocation: number[][] = [];
-  private enemyCastleLoc:number [][] = [];
-  
+  private enemyCastleLoc: number[][] = [];
+
   public turn(): Action | Falsy {
     const choice: number[] = randomValidLoc(this);
-	// const enemyCastleLocation: number[][] = [];
 
     switch (this.me.unit) {
       case SPECS.PILGRIM: {
         // this.log("Pilgrim");
         return this.handlePilgrim();
       }
-      
+
       case SPECS.CRUSADER: {
         // this.log(`Crusader health: ${this.me.health}`);
-		
-		// move torwards enemy castle
+
+        // move torwards enemy castle
         const attackingCoordinates = attackFirst(this);
 
         if (attackingCoordinates) {
@@ -49,6 +33,14 @@ class MyRobot extends BCAbstractRobot {
       }
 
       case SPECS.PROPHET: {
+        if (this.me.turn === 1) {
+          const horizontal = horizontalFlip(this);
+          this.enemyCastleLoc.push(enemyCastle(this.me.x, this.me.y, this.map.length, this, horizontal));
+          this.destination = this.enemyCastleLoc[0];
+          this.destinationQueue = simplePathFinder(this.map, [this.me.x, this.me.y], this.destination);
+          this.log("CASTLE LOCATION - PROPHET" + this.enemyCastleLoc[0][0] + ", " + this.enemyCastleLoc[0][1]);
+        }
+
         // this.log(`Prophet health: ${this.me.health}`);
         const attackingCoordinates = attackFirst(this);
 
@@ -56,20 +48,9 @@ class MyRobot extends BCAbstractRobot {
           return this.attack(attackingCoordinates[0], attackingCoordinates[1]);
         }
 
-		if(this.me.turn === 1)
-		{
-			const horizontal = horizontalFlip(this); 
-			this.enemyCastleLoc.push(enemyCastle(this.me.x, this.me.y, this.map.length, this, horizontal));
-			this.destination = this.enemyCastleLoc[0];
-			this.destinationQueue = simplePathFinder(this.map, [this.me.x, this.me.y], this.destination);
-			this.log("CASTE LOCATION PROPHET" + this.enemyCastleLoc[0][0] + ", " + this.enemyCastleLoc[0][1]);
-		}
-		
-		
-		if(this.enemyCastleLoc !== null && (this.destinationQueue !== undefined || this.destinationQueue.length !== 0))
-		{
-		  return rushCastle(this, this.destination, this.destinationQueue);
-		}
+        if (this.enemyCastleLoc !== null && (this.destinationQueue !== undefined || this.destinationQueue.length !== 0)) {
+          return rushCastle(this, this.destination, this.destinationQueue);
+        }
 
         return this.move(choice[0], choice[1]);
       }
@@ -84,13 +65,12 @@ class MyRobot extends BCAbstractRobot {
       }
 
       case SPECS.CASTLE: {
-		// get castle coordinates
-		if(this.me.turn === 1)
-		{
-			const horizontal = horizontalFlip(this); 
-			this.enemyCastleLoc.push(enemyCastle(this.me.x, this.me.y, this.map.length, this, horizontal));
-			this.log("CASTE LOCATION" + this.enemyCastleLoc[0][0] + ", " + this.enemyCastleLoc[0][1]);
-		}
+        // get castle coordinates
+        if (this.me.turn === 1) {
+          const horizontal = horizontalFlip(this);
+          this.enemyCastleLoc.push(enemyCastle(this.me.x, this.me.y, this.map.length, this, horizontal));
+          this.log("CASTLE LOCATION" + this.enemyCastleLoc[0][0] + ", " + this.enemyCastleLoc[0][1]);
+        }
         return this.handleCastle();
       }
     }
@@ -98,13 +78,13 @@ class MyRobot extends BCAbstractRobot {
 
   private handleCastle(): Action | Falsy {
     // Castle build pilgrims at first 2 turns
-    if (this.me.turn < 3){
-      const buildLoc: number[] = randomValidLoc(this);
+    if (this.me.turn < 3) {
+      const buildLoc: number[] = availableLoc(this.me.x, this.me.y, this.getVisibleRobotMap());
       // Have each castle build pilgrims in first 2 turns
       this.log(`Building a pilgrim at (${buildLoc[0]}, ${buildLoc[1]}) turn (${this.me.turn})`);
       return this.buildUnit(SPECS.PILGRIM, buildLoc[0], buildLoc[1]);
     }
-    
+
     // If castle can't build, it tries to attack
     if (this.karbonite >= 10) {
       return castleBuild(this);
@@ -114,7 +94,7 @@ class MyRobot extends BCAbstractRobot {
       return this.attack(attackingCoordinates[0], attackingCoordinates[1]);
     }
   }
-  
+
   private handlePilgrim(): Action | Falsy {
     this.log(" > > > PILGRIM TIME > > >");
     // let action: Action | Falsy = undefined;
@@ -122,7 +102,7 @@ class MyRobot extends BCAbstractRobot {
       this.initializePilgrim();
     }
 
-    if(this.destination === undefined) {
+    if (this.destination === undefined) {
       // Calculate closest karbonite/fuel location.
       this.log(`MY DEST IS ${this.resourceLocation}`)
       this.destination = this.resourceLocation;
@@ -133,7 +113,7 @@ class MyRobot extends BCAbstractRobot {
       this.log(` > > > NEXT MOVE ${this.nextMove}> > >`);
     }
 
-    if(this.me.karbonite === 20 || this.me.fuel === 100) {
+    if (this.me.karbonite === 20 || this.me.fuel === 100) {
       // TODO: Make pilgrim walk back to castle if inventory is full.
       this.log("---FULL INVENTORY, RETURNING TO BASE---");
       this.goMining = false;
@@ -143,7 +123,7 @@ class MyRobot extends BCAbstractRobot {
       const dist = Math.pow(dx, 2) + Math.pow(dy, 2);
 
       // If castle is in adjacent square, give resources
-      if (dist <= 2){
+      if (dist <= 2) {
         this.log(`GIVING RESOURCES TO CASTLE [${dx},${dy}] AWAY`);
 
         return this.give(dx, dy, this.me.karbonite, this.me.fuel);
@@ -151,12 +131,12 @@ class MyRobot extends BCAbstractRobot {
       const validLoc = simpleValidLoc(this);
       this.destination = [closestCastle[0] + validLoc[0], closestCastle[1] + validLoc[1]];
     }
-    
-    if(this.me.x === this.destination[0] && this.me.y === this.destination[1]) {
+
+    if (this.me.x === this.destination[0] && this.me.y === this.destination[1]) {
       // If on destination and is going mining, mine.
-      if(this.goMining === true) {
+      if (this.goMining === true) {
         this.log("CURRENTLY MINING");
-        return(this.mine());
+        return (this.mine());
       }
       this.destination = undefined;
     }
@@ -165,7 +145,7 @@ class MyRobot extends BCAbstractRobot {
       // return pilgrimBuild(this);
     }
 
-    if((this.me.x !== this.nextMove[0]) && (this.me.y !== this.nextMove[1])) {
+    if ((this.me.x !== this.nextMove[0]) && (this.me.y !== this.nextMove[1])) {
       // TODO: Possibly move this into a separate function?
       const moveX = this.nextMove[0] - this.me.x;
       const moveY = this.nextMove[1] - this.me.y;
@@ -176,7 +156,7 @@ class MyRobot extends BCAbstractRobot {
       return this.move(moveX, moveY);
     }
 
-    if(this.destinationQueue.length !== 0 && ((this.me.x === this.nextMove[0]) && (this.me.y === this.nextMove[1]))) {
+    if (this.destinationQueue.length !== 0 && ((this.me.x === this.nextMove[0]) && (this.me.y === this.nextMove[1]))) {
       // If the destination queue has coordinates and my current location is the 
       // same as my next move's location, then pop next destination and set nextMove to it.
       this.nextMove = this.destinationQueue.pop();
@@ -187,13 +167,13 @@ class MyRobot extends BCAbstractRobot {
     }
   }
 
-  // TODO: Fix bug. Resource locations are only being set to fuel
+  // Sets pilgrims' initial mining job
   private initializePilgrim() {
     this.log("> > > FINDING THINGS > > >")
     // 1st pilgrim mines karbonite. 2nd pilgrim mines fuel
     this.resourceLocation = (visiblePilgrims(this) <= 1) ?
-    closestMiningLocation([this.me.x, this.me.y], this.karbonite_map) :
-    closestMiningLocation([this.me.x, this.me.y], this.fuel_map);
+      closestMiningLocation([this.me.x, this.me.y], this.karbonite_map) :
+      closestMiningLocation([this.me.x, this.me.y], this.fuel_map);
     this.log(`VISPILGS < 1: ${visiblePilgrims(this) < 1} RESRC LOC: ${this.resourceLocation}, pilnum${visiblePilgrims(this)}`);
   }
 }
