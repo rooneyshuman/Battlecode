96646ab869311ae7777a47e7a5d67529c0e3ce92
rooneyshuman <rooneyshuman@users.noreply.github.com>
2019-02-13 03:23:13 -0800

    Castles build new prilgrims every 100 turns

--- src/BuildUnits.ts
+++ src/BuildUnits.ts
@@ -3,20 +3,26 @@ import { availableLoc, visiblePilgrims } from "./utils";
 
 export function castleBuild(self: BCAbstractRobot): BuildAction | Falsy {
     const visionMap = self.getVisibleRobotMap();
-    const buildLoc: number[] = availableLoc(self.me.x, self.me.y, visionMap);
+    const buildLoc: number[] = availableLoc(self.me.x, self.me.y, visionMap, self.map);
 
     self.log(`Castle health: ${self.me.health}`);
 
      // Pilgrims have been killed off, build new ones
-     if (visiblePilgrims(self) < 2) {
+     if (visiblePilgrims(self) < 2 && buildLoc) {
         self.log(`Building a pilgrim at (${buildLoc[0]}, ${buildLoc[1]}) turn (${self.me.turn})`);
         return self.buildUnit(SPECS.PILGRIM, buildLoc[0], buildLoc[1]);
     }
 
-    // Repeat while castle has enough karbonite for at least one pilgrim
-    if (self.karbonite >= 25) {
+    // Check if open location and if enough karb for prophet
+    if (self.karbonite >= 25 && buildLoc) {
         self.log(`Building a prophet at (${buildLoc[0]}, ${buildLoc[1]}) turn (${self.me.turn})`);
         return self.buildUnit(SPECS.PROPHET, buildLoc[0], buildLoc[1]);
+    }
+
+    // Check if open location and enough karb for pilgrim 
+    else if (self.karbonite >= 10 && buildLoc && self.me.turn % 100){
+        self.log(`Building a pilgrim at (${buildLoc[0]}, ${buildLoc[1]}) turn (${self.me.turn})`);
+        return self.buildUnit(SPECS.PILGRIM, buildLoc[0], buildLoc[1]);
     }
 }
 
